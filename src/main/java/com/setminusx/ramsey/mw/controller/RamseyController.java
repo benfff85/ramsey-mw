034a3ce8065e4be4cfa6862f861fe85741742ad9
Ben Ferenchak <benfff85@users.noreply.github.com>
2019-06-20 22:41:16 -0400

    Lombok Integration (#4)
    Lombok Integration

--- src/main/java/com/setminusx/ramsey/mw/controller/RamseyController.java
+++ src/main/java/com/setminusx/ramsey/mw/controller/RamseyController.java
@@ -2,20 +2,24 @@ package com.setminusx.ramsey.mw.controller;
 
 import com.setminusx.ramsey.mw.dto.GraphDTO;
 import com.setminusx.ramsey.mw.service.GraphService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class RamseyController {
 
-    @Autowired private GraphService graphService;
+    @Autowired
+    private GraphService graphService;
 
     @GetMapping("/api/ramsey/graphs")
-    public ResponseEntity<List<GraphDTO>> getGraphByType(@RequestParam() String type, @RequestParam(defaultValue = "1") String count) {
+    public ResponseEntity<List<GraphDTO>> getGraphByType(@RequestParam() String type, @RequestParam(defaultValue = "1") String count, @RequestHeader() String rqid) {
 
+        log.info("Processing getGraphByType for rqid: " + rqid);
         if ("min".equals(type)) {
             List<GraphDTO> graphs = graphService.getGraphsWithMinCliqueCount(Integer.parseInt(count));
             return ResponseEntity.ok().body(graphs);
@@ -25,11 +29,12 @@ public class RamseyController {
     }
 
     @GetMapping("/api/ramsey/graphs/{id}")
-    public ResponseEntity<GraphDTO> getGraphById(@PathVariable(value = "id") Integer id) {
+    public ResponseEntity<GraphDTO> getGraphByGraphId(@PathVariable(value = "id") Integer id, @RequestHeader() String rqid) {
 
-        GraphDTO graphDto = null;
+        log.info("Processing getGraphByGraphId for rqid: " + rqid);
+        GraphDTO graphDto;
         try {
-            graphDto = graphService.findGraphById(id);
+            graphDto = graphService.getGraphByGraphId(id);
         } catch (Exception e) {
             return ResponseEntity.notFound().header("Error-Description", "Graph with ID " + id + " not found").build();
         }
