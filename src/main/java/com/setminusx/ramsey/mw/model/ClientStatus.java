a620accec16d5552d5ddd7e72566ac7a51cd92fc
Ben Ferenchak <ben.ferenchak@gmail.com>
2024-09-15 17:00:18 -0400

    Updating middleware client APIs to provide other CRUD operations and to remove DTO/Entity segregation

--- src/main/java/com/setminusx/ramsey/mw/model/ClientStatus.java
+++
@@ -1,6 +0,0 @@
-package com.setminusx.ramsey.mw.model;
-
-public enum ClientStatus {
-    ACTIVE,
-    INACTIVE
-}
--- src/main/java/com/setminusx/ramsey/mw/repository/ClientRepo.java
+++ src/main/java/com/setminusx/ramsey/mw/repository/ClientRepo.java
@@ -1,13 +1,25 @@
 package com.setminusx.ramsey.mw.repository;
 
 import com.setminusx.ramsey.mw.entity.Client;
-import com.setminusx.ramsey.mw.model.ClientStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ClientRepo extends JpaRepository<Client, Integer> {
 
-    List<Client> findAllBySubgraphSizeAndVertexCountAndStatus(Integer subgraphSize, Integer vertexCount, ClientStatus status);
+    @Query("SELECT c FROM Client c WHERE " +
+            "(:subgraphSize IS NULL OR c.subgraphSize = :subgraphSize) AND " +
+            "(:vertexCount IS NULL OR c.vertexCount = :vertexCount) AND " +
+            "(:status IS NULL OR c.status = :status) AND " +
+            "(:type IS NULL OR c.type = :type)")
+    List<Client> findBySubgraphSizeAndVertexCountAndStatusAndType(
+            @Param("subgraphSize") Integer subgraphSize,
+            @Param("vertexCount") Integer vertexCount,
+            @Param("status") Client.Status status,
+            @Param("type") Client.Type type);
 
 }
