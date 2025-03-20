33c0c993db238b4f9c3c3cc65b16e0bc832df4d7
Ben Ferenchak <ben.ferenchak@gmail.com>
2025-03-20 12:57:36 -0400

    Removed DTO from Work Unit endpoints for speed and simplicity

--- src/main/java/com/setminusx/ramsey/mw/service/WorkUnitService.java
+++ src/main/java/com/setminusx/ramsey/mw/service/WorkUnitService.java
@@ -1,6 +1,5 @@
 package com.setminusx.ramsey.mw.service;
 
-import com.setminusx.ramsey.mw.dto.WorkUnitDto;
 import com.setminusx.ramsey.mw.entity.WorkUnit;
 import com.setminusx.ramsey.mw.model.WorkUnitStatus;
 import com.setminusx.ramsey.mw.repository.WorkUnitRepo;
@@ -15,70 +14,34 @@ import java.util.List;
 @Service
 public class WorkUnitService {
 
+    private final WorkUnitRepo workUnitRepo;
     @Value("${ramsey.work-unit.page-size.default}")
     private Integer defaultPageSize;
-
-    private final WorkUnitRepo workUnitRepo;
 
 
     public WorkUnitService(WorkUnitRepo workUnitRepo) {
         this.workUnitRepo = workUnitRepo;
     }
 
-    public List<WorkUnitDto> saveAll(List<WorkUnitDto> workUnitDtos) {
-        List<WorkUnit> workUnits = workUnitDtos.stream().map(WorkUnitService::mapDTOToWorkUnit).toList();
-        List<WorkUnit> createdWorkUnits = workUnitRepo.saveAll(workUnits);
-        return createdWorkUnits.stream().map(WorkUnitService::mapWorkUnitToDTO).toList();
+
+    public List<WorkUnit> getWorkUnits(WorkUnitStatus status, Integer vertexCount, Integer subgraphSize, String assignedClientId, Integer pageSize) {
+        return workUnitRepo.findBySubgraphSizeAndVertexCountAndStatusAndAssignedClient(subgraphSize, vertexCount, status, assignedClientId, getPageable(pageSize));
     }
 
-    public List<WorkUnitDto> getAll(WorkUnitStatus status, Integer vertexCount, Integer subgraphSize, Integer pageSize) {
-        return workUnitRepo.findAllBySubgraphSizeAndVertexCountAndStatus(subgraphSize, vertexCount, status, getPageable(pageSize)).stream().map(WorkUnitService::mapWorkUnitToDTO).toList();
+    public WorkUnit getWorkUnitById(Integer id) {
+        return workUnitRepo.findById(id).orElse(null);
     }
 
-    public List<WorkUnitDto> getAllAssignedToClient(WorkUnitStatus status, Integer vertexCount, Integer subgraphSize, String assignedClientId, Integer pageSize) {
-        return workUnitRepo.findAllBySubgraphSizeAndVertexCountAndStatusAndAssignedClient(subgraphSize, vertexCount, status, assignedClientId, getPageable(pageSize)).stream().map(WorkUnitService::mapWorkUnitToDTO).toList();
+    public List<WorkUnit> createOrUpdateWorkUnits(List<WorkUnit> workUnits) {
+        return workUnitRepo.saveAll(workUnits);
     }
 
-    public List<WorkUnitDto> getMostRecentForGraphId(Integer graphId) {
-        return workUnitRepo.findAllByBaseGraphIdOrderByIdDesc(graphId, getPageable(1)).stream().map(WorkUnitService::mapWorkUnitToDTO).toList();
+    public void deleteWorkUnit(Integer id) {
+        workUnitRepo.deleteById(id);
     }
 
-    private static WorkUnit mapDTOToWorkUnit(WorkUnitDto workUnitDto) {
-        WorkUnit workUnit = new WorkUnit();
-        workUnit.setAssignedDate(workUnitDto.getAssignedDate());
-        workUnit.setBaseGraphId(workUnitDto.getBaseGraphId());
-        workUnit.setCliqueCount(workUnitDto.getCliqueCount());
-        workUnit.setCompletedDate(workUnitDto.getCompletedDate());
-        workUnit.setProcessingStartedDate(workUnitDto.getProcessingStartedDate());
-        workUnit.setCreatedDate(workUnitDto.getCreatedDate());
-        workUnit.setEdgesToFlip(workUnitDto.getEdgesToFlip());
-        workUnit.setId(workUnitDto.getId());
-        workUnit.setStatus(workUnitDto.getStatus());
-        workUnit.setSubgraphSize(workUnitDto.getSubgraphSize());
-        workUnit.setVertexCount(workUnitDto.getVertexCount());
-        workUnit.setAssignedClient(workUnitDto.getAssignedClient());
-        workUnit.setPriority(workUnitDto.getPriority());
-        workUnit.setWorkUnitAnalysisType(workUnitDto.getWorkUnitAnalysisType());
-        return workUnit;
-    }
-
-    private static WorkUnitDto mapWorkUnitToDTO(WorkUnit workUnit) {
-        WorkUnitDto workUnitDto = new WorkUnitDto();
-        workUnitDto.setAssignedDate(workUnit.getAssignedDate());
-        workUnitDto.setBaseGraphId(workUnit.getBaseGraphId());
-        workUnitDto.setCliqueCount(workUnit.getCliqueCount());
-        workUnitDto.setCompletedDate(workUnit.getCompletedDate());
-        workUnitDto.setProcessingStartedDate(workUnit.getProcessingStartedDate());
-        workUnitDto.setCreatedDate(workUnit.getCreatedDate());
-        workUnitDto.setEdgesToFlip(workUnit.getEdgesToFlip());
-        workUnitDto.setId(workUnit.getId());
-        workUnitDto.setStatus(workUnit.getStatus());
-        workUnitDto.setSubgraphSize(workUnit.getSubgraphSize());
-        workUnitDto.setVertexCount(workUnit.getVertexCount());
-        workUnitDto.setAssignedClient(workUnit.getAssignedClient());
-        workUnitDto.setPriority(workUnit.getPriority());
-        workUnitDto.setWorkUnitAnalysisType(workUnit.getWorkUnitAnalysisType());
-        return workUnitDto;
+    public List<WorkUnit> getMostRecentForGraphId(Integer graphId) {
+        return workUnitRepo.findAllByBaseGraphIdOrderByIdDesc(graphId, getPageable(1));
     }
 
     private Pageable getPageable(Integer pageSize) {
