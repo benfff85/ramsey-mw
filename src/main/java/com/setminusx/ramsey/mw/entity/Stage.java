46f7f0a56e197c07e8a12e086c503a164912f868
Ben Ferenchak <ben.ferenchak@gmail.com>
2026-01-19 10:06:46 -0500

    feat: add work enumeration strategy fields to Stage and Campaign entities

--- src/main/java/com/setminusx/ramsey/mw/entity/Stage.java
+++ src/main/java/com/setminusx/ramsey/mw/entity/Stage.java
@@ -20,6 +20,12 @@ public class Stage {
     private Integer campaignId;
     private Integer latestWorkUnitId;
 
+    @Enumerated(EnumType.STRING)
+    private WorkEnumerationStrategy workEnumerationStrategy;
+
+    @Column(columnDefinition = "TEXT")
+    private String details;
+
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
 
@@ -37,6 +43,12 @@ public class Stage {
     public enum Status {
         ACTIVE,
         INACTIVE
+    }
+
+    public enum WorkEnumerationStrategy {
+        BASIC,
+        SINGLE_EDGE_CARDINALITY,
+        DUAL_EDGE_CARDINALITY
     }
 
 }
